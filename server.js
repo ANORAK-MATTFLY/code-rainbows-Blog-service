727f408fdf3c2f49a83c1f491609b6371cc1f916
ANORAK-MATTFLY <benmatanda354@gmail.com>
2021-04-23 12:52:49 +0100

    add federation logic on the blog service

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
+
 const { ApolloServer } = require("apollo-server");
+const { buildFederatedSchema } = require('@apollo/federation');
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const express = require("express");
-const expressServer = express();
 
-expressServer.use(bodyParser.json())
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    expressServer,
-    playground: {
-        endpoint: '/graphql',
-    },
-    context: (({ req, res }) => {
-        return { res, req };
-    }),
-});
 
 const mongoConfig = {
     useNewUrlParser: true,
@@ -28,6 +15,11 @@ const mongoConfig = {
     useFindAndModify: false,
 };
 
+const server = new ApolloServer({
+    schema: buildFederatedSchema([{ typeDefs, resolvers }])
+});
+
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ojnan.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, mongoConfig).then(() =>
     server.listen(4001).then(({ url }) => {
         console.log(`🚀 Server ready at ${url}`);
@@ -35,3 +27,6 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 ).catch(error => {
     throw new Error(error);
 });
+
+
+
