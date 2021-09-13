f1f9828d71e7597ea9af6bf32371a2ace05bd525
Ben Matanda <benmatanda354@gmail.com>
2021-09-13 16:04:06 +0100

    initial apollo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 9100;
+    port = 4002;
 }
 
 const mongoConfig = {
@@ -24,7 +24,7 @@ const server = new ApolloServer({
 
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ojnan.mongodb.net/blog${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, mongoConfig).then(() =>
-    server.listen(port || 5000).then(({ url }) => {
+    server.listen(port).then(({ url }) => {
         console.log(`"https://rainbow-analytics-api.herokuapp.com/graphql"`);
     })
 ).catch(error => {
